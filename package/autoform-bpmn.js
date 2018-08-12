a09d65fa6439d8003a99b0f54ee30baae116d7ac
Jan Küster <jkuester@uni-bremen.de>
2018-08-12 21:17:12 +0200

    use standardjs

--- package/autoform-bpmn.js
+++ package/autoform-bpmn.js
@@ -1,187 +1,188 @@
-import { Template } from 'meteor/templating';
-import { Random } from 'meteor/random';
-import { ReactiveVar } from 'meteor/reactive-var';
-import { $ } from 'meteor/jquery';
-import BpmnModeler from 'bpmn-js/lib/Modeler';
-import BpmnViewer from 'bpmn-js/lib/NavigatedViewer';
-import propertiesPanelModule from 'bpmn-js-properties-panel';
-import propertiesProviderModule from 'bpmn-js-properties-panel/lib/provider/camunda';
-import camundaModdleDescriptor from 'camunda-bpmn-moddle/resources/camunda';
+import { Template } from 'meteor/templating'
+import { Random } from 'meteor/random'
+import { ReactiveVar } from 'meteor/reactive-var'
+import { $ } from 'meteor/jquery'
+import { _ } from 'meteor/underscore'
+import BpmnModeler from 'bpmn-js/lib/Modeler'
+import BpmnViewer from 'bpmn-js/lib/NavigatedViewer'
+import propertiesPanelModule from 'bpmn-js-properties-panel'
+import propertiesProviderModule from 'bpmn-js-properties-panel/lib/provider/camunda'
+import camundaModdleDescriptor from 'camunda-bpmn-moddle/resources/camunda'
 
 // import 'diagram-js/assets/diagram-js.css';
-import 'bpmn-js/dist/assets/diagram-js.css';
-import 'bpmn-js/dist/assets/bpmn-font/css/bpmn-embedded.css';
+import 'bpmn-js/dist/assets/diagram-js.css'
+import 'bpmn-js/dist/assets/bpmn-font/css/bpmn-embedded.css'
 
-import { Utils } from './autoform-bpmn-utils'; // expose it to make it testable
-import './autoform-bpmn.less';
+import { Utils } from './autoform-bpmn-utils' // expose it to make it testable
+import './autoform-bpmn.less'
 
-BpmnModelerUtils = Utils; // eslint-disable-line no-undef
-
+BpmnModelerUtils = Utils // eslint-disable-line no-undef
 
 // extend autoform with bpmn modeler
 AutoForm.addInputType('bpmn', {
   template: 'afBpmn',
-  valueOut() {
-    return this.val();
+  valueOut () {
+    return this.val()
   },
-  valueIn(initialValue) {
-    return initialValue;
-  },
-});
+  valueIn (initialValue) {
+    return initialValue
+  }
+})
 
 Template.afBpmn.onCreated(function () {
   // TODO add upload button and use this flag to indicate upload capabilities
   // TODO is this still required?
   // const uploadSupported = window.File && window.FileReader && window.FileList && window.Blob;
 
-  const instance = this;
-  instance.bpmnjsLoaded = new ReactiveVar(false);
-  instance.loadComplete = new ReactiveVar(false);
-  instance.disabled = new ReactiveVar(false);
-  instance.saving = new ReactiveVar(false);
+  const instance = this
+  instance.bpmnjsLoaded = new ReactiveVar(false)
+  instance.loadComplete = new ReactiveVar(false)
+  instance.disabled = new ReactiveVar(false)
+  instance.saving = new ReactiveVar(false)
 
   instance.model = new ReactiveVar(instance.data.value ||
-    Utils.createProcess(this.data.title || Random.id()));
+    Utils.createProcess(this.data.title || Random.id()))
 
-  instance.currentTarget = new ReactiveVar(false);
+  instance.currentTarget = new ReactiveVar(false)
 
-  const { atts } = this.data;
+  const { atts } = this.data
   if (Object.hasOwnProperty.call(atts, 'disabled')) {
-    instance.disabled.set(true);
+    instance.disabled.set(true)
   }
 
-  instance.key = new ReactiveVar(atts['data-schema-key'] || '');
-  instance.mapping = new ReactiveVar(atts.mapping);
+  instance.key = new ReactiveVar(atts['data-schema-key'] || '')
+  instance.mapping = new ReactiveVar(atts.mapping)
 
   // TODO use extension instead of replacing?
   // instance.serviceProvider = atts.service;
 
   instance.autorun(function () {
     if (instance.bpmnjsLoaded.get()) {
-      Utils.modeler.on('element.click', Utils.onElementClick.bind(instance));
+      Utils.modeler.on('element.click', Utils.onElementClick.bind(instance))
       Utils.modeler.importXML(instance.model.get(), function (err, res) {
         if (res) {
-          Utils.container.removeClass('with-error').addClass('with-diagram');
-          instance.loadComplete.set(true);
-          $('#af-bpmn-model-input').val(instance.model.get());
+          Utils.container.removeClass('with-error').addClass('with-diagram')
+          instance.loadComplete.set(true)
+          $('#af-bpmn-model-input').val(instance.model.get())
         }
-      });
+        if (err) console.error(err)
+      })
     }
-  });
-});
+  })
+})
 
 Template.afBpmn.onRendered(function () {
-  const instance = this;
-  const disabled = instance.disabled.get();
+  const instance = this
+  const disabled = instance.disabled.get()
 
   if (!instance._rendered) {
-    instance._rendered = true;
+    instance._rendered = true
 
-    Utils.canvas = $('#af-bpmn-canvas');
-    Utils.container = $('#af-bpmn-drop-zone');
-    Utils.propertiesParent = $('#af-bpmn-properties-panel');
+    Utils.canvas = $('#af-bpmn-canvas')
+    Utils.container = $('#af-bpmn-drop-zone')
+    Utils.propertiesParent = $('#af-bpmn-properties-panel')
 
     if (disabled) {
       Utils.modeler = new BpmnViewer({
         container: Utils.canvas,
         // make camunda prefix known for import, editing and export
         moddleExtensions: {
-          camunda: camundaModdleDescriptor,
-        },
-      });
+          camunda: camundaModdleDescriptor
+        }
+      })
     } else {
       Utils.modeler = new BpmnModeler({
         container: Utils.canvas,
         additionalModules: [
           propertiesPanelModule,
-          propertiesProviderModule,
+          propertiesProviderModule
         ],
         propertiesPanel: {
-          parent: Utils.propertiesParent,
+          parent: Utils.propertiesParent
         },
         // make camunda prefix known for import, editing and export
         moddleExtensions: {
-          camunda: camundaModdleDescriptor,
-        },
-      });
-
+          camunda: camundaModdleDescriptor
+        }
+      })
 
       Utils.modeler.on('commandStack.changed', _.debounce(function (/* evt */) {
         Utils.saveDiagram(function (err, xml) {
-          $('#af-bpmn-model-input').val(xml);
-        });
-        return true;
-      }, 500));
+          if (err) console.error(err)
+          if (xml) $('#af-bpmn-model-input').val(xml)
+        })
+        return true
+      }, 500))
     }
   }
 
   if (disabled && Utils.canvas && Utils.container) {
-    this.bpmnjsLoaded.set(true);
+    this.bpmnjsLoaded.set(true)
   }
   if (!disabled && Utils.canvas && Utils.container && Utils.propertiesParent && !!$('.bpp-properties-panel')[0]) {
-    this.bpmnjsLoaded.set(true);
+    this.bpmnjsLoaded.set(true)
   }
-});
+})
 
 Template.afBpmn.helpers({
-  dataSchemaKey() {
-    return Template.instance().key.get();
+  dataSchemaKey () {
+    return Template.instance().key.get()
   },
-  loadComplete() {
+  loadComplete () {
     return Template.instance().loadComplete.get() &&
-      Template.instance().loadComplete.get();
+      Template.instance().loadComplete.get()
   },
-  disabled() {
-    return Template.instance().disabled.get();
-  },
-});
-
+  disabled () {
+    return Template.instance().disabled.get()
+  }
+})
 
 Template.afBpmn.events({
-  'submit'(event) {
-    event.preventDefault();
-    event.stopPropagation();
+  'submit' (event) {
+    event.preventDefault()
+    event.stopPropagation()
   },
-  'change #af-bpmn-file-upload'(event, templateInstance) {
-    const disabled = templateInstance.disabled.get();
-    if (disabled) return;
+  'change #af-bpmn-file-upload' (event, templateInstance) {
+    const disabled = templateInstance.disabled.get()
+    if (disabled) return
 
-    const target = $('#af-bpmn-file-upload').get(0);
-    const { files } = target;
+    const target = $('#af-bpmn-file-upload').get(0)
+    const { files } = target
     if (files && files[0]) {
-      const file = files[0];
-      const reader = new FileReader();
-
+      const file = files[0]
+      const reader = new FileReader()
 
       // Closure to capture the file information.
       reader.onloadend = function (result) {
-        const importXml = result && result.currentTarget && result.currentTarget.result;
+        const importXml = result && result.currentTarget && result.currentTarget.result
         if (importXml) {
           Utils.modeler.importXML(importXml, function (err, res) {
-            if (err) Utils.modeler.importXML(templateInstance.model.get());
-            if (res) $('#af-bpmn-model-input').val(importXml);
-          });
+            if (err) Utils.modeler.importXML(templateInstance.model.get())
+            if (res) $('#af-bpmn-model-input').val(importXml)
+          })
         }
-      };
+      }
 
       // Read in the image file as a data URL.
-      reader.readAsText(file);
+      reader.readAsText(file)
     }
   },
-  'click #af-bpmn-download-diagram'(event) {
-    event.preventDefault();
-    event.stopPropagation();
+  'click #af-bpmn-download-diagram' (event) {
+    event.preventDefault()
+    event.stopPropagation()
 
     Utils.saveDiagram(function (err, xml) {
-      Utils.saveFile(' application/xml', 'diagram.bpmn', xml);
-    });
+      if (err) console.error(err)
+      if (xml) Utils.saveFile(' application/xml', 'diagram.bpmn', xml)
+    })
   },
-  'click #af-bpmn-download-svg'(event) {
-    event.preventDefault();
-    event.stopPropagation();
+  'click #af-bpmn-download-svg' (event) {
+    event.preventDefault()
+    event.stopPropagation()
 
     Utils.saveSVG(function (err, svg) {
-      Utils.saveFile('image/svg+xml', 'diagram.svg', svg);
-    });
-  },
-});
+      if (err) console.error(err)
+      if (svg) Utils.saveFile('image/svg+xml', 'diagram.svg', svg)
+    })
+  }
+})
--- tests/autoform-bpmn.tests.js
+++ tests/autoform-bpmn.tests.js
@@ -1,140 +1,138 @@
-// import { _ } from 'meteor/underscore';
-import { Template } from 'meteor/templating';
-import { assert } from 'meteor/practicalmeteor:chai';
-import { Random } from 'meteor/random';
-import SimpleSchema from 'simpl-schema';
-import { BpmnModelerUtils as Utils } from 'meteor/jkuester:autoform-bpmn';
+import { Template } from 'meteor/templating'
+import { assert } from 'meteor/practicalmeteor:chai'
+import { Random } from 'meteor/random'
+import SimpleSchema from 'simpl-schema'
+import { BpmnModelerUtils as Utils } from 'meteor/jkuester:autoform-bpmn'
 
-import { renderPizza, getIsRendered, getMultipleIsRendered, withRenderedTemplate } from './testutils.tests';
-import { pizza } from './pizza';
+import { renderPizza, getIsRendered, getMultipleIsRendered, withRenderedTemplate } from './testutils.tests'
+import { pizza } from './pizza'
 
-SimpleSchema.extendOptions(['autoform']);
+SimpleSchema.extendOptions(['autoform'])
 
-
-const coverageUrlId = 'afBbpmn-coverage-target'; // used as target for new tab
+const coverageUrlId = 'afBbpmn-coverage-target' // used as target for new tab
 
 describe('autoform-bpmn', function () {
   beforeEach(function () {
-    Template.registerHelper('_', key => key);
-  });
+    Template.registerHelper('_', key => key)
+  })
 
   afterEach(function () {
-    Template.deregisterHelper('_');
-  });
+    Template.deregisterHelper('_')
+  })
 
   describe('utils', function () {
     it('createProcess', function () {
-      const randomId = Random.id();
-      const processXml = Utils.createProcess(randomId);
-      assert.isAbove(processXml.indexOf(randomId), -1);
-    });
+      const randomId = Random.id()
+      const processXml = Utils.createProcess(randomId)
+      assert.isAbove(processXml.indexOf(randomId), -1)
+    })
 
     it('setEncoded (with data)', function () {
-      const randomId = Random.id();
-      const processXml = Utils.createProcess(randomId);
-      const encodedData = encodeURIComponent(processXml);
-      const enabledLink = $('<a>');
-      const enabledName = 'enabled_link';
-      Utils.setEncoded(enabledLink, enabledName, processXml);
-      assert.equal(enabledLink.prop('disabled'), false);
-      assert.equal(enabledLink.attr('download'), enabledName);
-      const href = enabledLink.attr('href');
-      assert.isAbove(href.indexOf(encodedData), -1);
-    });
+      const randomId = Random.id()
+      const processXml = Utils.createProcess(randomId)
+      const encodedData = encodeURIComponent(processXml)
+      const enabledLink = $('<a>')
+      const enabledName = 'enabled_link'
+      Utils.setEncoded(enabledLink, enabledName, processXml)
+      assert.equal(enabledLink.prop('disabled'), false)
+      assert.equal(enabledLink.attr('download'), enabledName)
+      const href = enabledLink.attr('href')
+      assert.isAbove(href.indexOf(encodedData), -1)
+    })
 
     it('setEncoded (no data)', function () {
-      const disabledLink = $('<a></a>');
-      Utils.setEncoded(disabledLink, '', null);
-      assert.equal(disabledLink.prop('disabled'), true);
-      assert.isUndefined(disabledLink.attr('download'));
-      assert.isUndefined(disabledLink.attr('href'));
-    });
+      const disabledLink = $('<a></a>')
+      Utils.setEncoded(disabledLink, '', null)
+      assert.equal(disabledLink.prop('disabled'), true)
+      assert.isUndefined(disabledLink.attr('download'))
+      assert.isUndefined(disabledLink.attr('href'))
+    })
 
     it('saveDiagram', function (done) {
       Utils.modeler = {
-        saveXML(obj, cb) {
-          assert.isTrue(obj.format);
-          cb();
-        },
-      };
+        saveXML (obj, cb) {
+          assert.isTrue(obj.format)
+          cb()
+        }
+      }
       Utils.saveDiagram(() => {
-        done();
-      });
-    });
+        done()
+      })
+    })
 
     it('saveSVG', function (done) {
       Utils.modeler = {
-        saveSVG(cb) {
-          cb();
-        },
-      };
+        saveSVG (cb) {
+          cb()
+        }
+      }
       Utils.saveSVG(() => {
-        done();
-      });
-    });
+        done()
+      })
+    })
 
     it('onElementClick', function () {
       const instance = {
         currentTarget: {
           value: null,
-          set(value) {
-            this.value = value;
-          },
-        },
-      };
-      const id = Random.id();
-      const bound = Utils.onElementClick.bind(instance);
+          set (value) {
+            this.value = value
+          }
+        }
+      }
+      const id = Random.id()
+      const bound = Utils.onElementClick.bind(instance)
       bound({
         element: {
-          businessObject: id,
-        },
-      });
+          businessObject: id
+        }
+      })
 
-      assert.equal(instance.currentTarget.value, id);
-    });
-  });
+      assert.equal(instance.currentTarget.value, id)
+    })
+  })
 
   describe('default render', function () {
     const data = {
       atts: {
-        'data-schema-key': 'workflowData',
+        'data-schema-key': 'workflowData'
 
       },
-      value: undefined,
-    };
+      value: undefined
+    }
 
     it('renders the hidden inputs', function (done) {
       withRenderedTemplate('afBpmn', data, (el) => {
-        const hiddenInput = $($(el).find('#af-bpmn-model-input')[0]);
-        assert.equal(hiddenInput.attr('type'), 'hidden');
-        assert.equal(hiddenInput.attr('data-schema-key'), data.atts['data-schema-key']);
-        done();
-      });
-    });
+        const hiddenInput = $($(el).find('#af-bpmn-model-input')[0])
+        assert.equal(hiddenInput.attr('type'), 'hidden')
+        assert.equal(hiddenInput.attr('data-schema-key'), data.atts['data-schema-key'])
+        done()
+      })
+    })
 
     it('renders all containers', function (done) {
       withRenderedTemplate('afBpmn', data, (el) => {
-        const template = $(el);
-        const rendersAll = getMultipleIsRendered(template);
+        const template = $(el)
+        const rendersAll = getMultipleIsRendered(template)
 
         const containers = [
           '.bjs-container',
           '.djs-container',
           '.djs-tooltip-container',
-          '.djs-overlay-container',
-        ];
-        rendersAll(containers, 1);
+          '.djs-overlay-container'
+        ]
+        rendersAll(containers, 1)
 
         // svg content
         // isRendered('svg', 1);
-        done();
-      });
-    });
+        done()
+      })
+    })
 
     it('renders all tools', function (done) {
       withRenderedTemplate('afBpmn', data, (el) => {
-        const template = $(el);
-        const rendersAll = getMultipleIsRendered(template);
+        const template = $(el)
+        const rendersAll = getMultipleIsRendered(template)
 
         const tools = [
           '.djs-palette',
@@ -150,37 +148,36 @@ describe('autoform-bpmn', function () {
           '.bpmn-icon-subprocess-expanded',
           '.bpmn-icon-data-object',
           '.bpmn-icon-data-store',
-          '.bpmn-icon-participant',
-        ];
-        rendersAll(tools, 1);
+          '.bpmn-icon-participant'
+        ]
+        rendersAll(tools, 1)
 
-        done();
-      });
-    });
+        done()
+      })
+    })
 
     it('renders the properties panel and all its children', function (done) {
       withRenderedTemplate('afBpmn', data, (el) => {
-        const template = $(el);
-        const rendersAll = getMultipleIsRendered(template);
-
+        const template = $(el)
+        const rendersAll = getMultipleIsRendered(template)
 
         const properties = [
           '.bpp-properties-panel',
           '.bpp-properties',
           '.bpp-properties-header',
           '.bpp-properties-tab-bar',
-          '.bpp-properties-tabs-links',
-        ];
-        rendersAll(properties, 1);
+          '.bpp-properties-tabs-links'
+        ]
+        rendersAll(properties, 1)
 
-        done();
-      });
-    });
+        done()
+      })
+    })
 
     it('renders the camunda moddle related properties as tab sections', function (done) {
       withRenderedTemplate('afBpmn', data, (el) => {
-        const template = $(el);
-        const rendersAll = getMultipleIsRendered(template);
+        const template = $(el)
+        const rendersAll = getMultipleIsRendered(template)
 
         const properties = [
           'a[data-tab-target="general"]',
@@ -190,91 +187,90 @@ describe('autoform-bpmn', function () {
           'a[data-tab-target="listeners"]',
           'a[data-tab-target="input-output"]',
           'a[data-tab-target="field-injections"]',
-          'a[data-tab-target="extensionElements"]',
-        ];
-        rendersAll(properties, 1);
+          'a[data-tab-target="extensionElements"]'
+        ]
+        rendersAll(properties, 1)
 
-        done();
-      });
-    });
+        done()
+      })
+    })
 
     it('renders action buttons', function (done) {
       withRenderedTemplate('afBpmn', data, (el) => {
-        const template = $(el);
-        const rendersAll = getMultipleIsRendered(template);
+        const template = $(el)
+        const rendersAll = getMultipleIsRendered(template)
 
         const buttons = [
           '#af-bpmn-download-diagram',
           '#af-bpmn-download-svg',
-          '#af-bpmn-file-upload',
-        ];
-        rendersAll(buttons, 1);
+          '#af-bpmn-file-upload'
+        ]
+        rendersAll(buttons, 1)
 
-        done();
-      });
-    });
+        done()
+      })
+    })
 
     it('renders a single process element (startevent) by default', function (done) {
       withRenderedTemplate('afBpmn', data, (el) => {
-        const svgRoot = $($(el).find('svg')[0]);
-        const rendersAll = getMultipleIsRendered(svgRoot);
+        const svgRoot = $($(el).find('svg')[0])
+        const rendersAll = getMultipleIsRendered(svgRoot)
 
         const elements = [
           '.djs-element',
-          '.djs-shape',
-        ];
-        rendersAll(elements, 2);
+          '.djs-shape'
+        ]
+        rendersAll(elements, 2)
 
         const elementAtts = [
           'g[data-element-id="StartEvent_1"]',
-          'g[data-element-id="StartEvent_1_label"]',
-        ];
+          'g[data-element-id="StartEvent_1_label"]'
+        ]
 
-        rendersAll(elementAtts, 1);
+        rendersAll(elementAtts, 1)
 
-
-        done();
-      });
-    });
-  });
+        done()
+      })
+    })
+  })
 
   describe('render with existing BPMN model', function () {
     const data = {
       atts: {
-        'data-schema-key': 'workflowData',
+        'data-schema-key': 'workflowData'
       },
-      value: pizza,
-    };
+      value: pizza
+    }
 
     it('renders an imported process diagram', function (done) {
-      renderPizza(data, done);
-    });
-  });
+      renderPizza(data, done)
+    })
+  })
 
   describe('render view-mode on disabled form', function () {
     const data = {
       atts: {
         'data-schema-key': 'workflowData',
-        disabled: '',
+        disabled: ''
       },
-      value: pizza,
-    };
+      value: pizza
+    }
 
     it('renders action buttons but not import', function (done) {
       withRenderedTemplate('afBpmn', data, (el) => {
-        const template = $(el);
-        const isRendered = getIsRendered(template);
-        isRendered('#af-bpmn-download-diagram', 1);
-        isRendered('#af-bpmn-download-svg', 1);
-        isRendered('#af-bpmn-file-upload', 0);
-        done();
-      });
-    });
+        const template = $(el)
+        const isRendered = getIsRendered(template)
+        isRendered('#af-bpmn-download-diagram', 1)
+        isRendered('#af-bpmn-download-svg', 1)
+        isRendered('#af-bpmn-file-upload', 0)
+        done()
+      })
+    })
 
     it('does not render the properties panel', function (done) {
       withRenderedTemplate('afBpmn', data, (el) => {
-        const template = $(el);
-        const rendersAll = getMultipleIsRendered(template);
+        const template = $(el)
+        const rendersAll = getMultipleIsRendered(template)
 
         const properties = [
           'a[data-tab-target="general"]',
@@ -284,22 +280,22 @@ describe('autoform-bpmn', function () {
           'a[data-tab-target="listeners"]',
           'a[data-tab-target="input-output"]',
           'a[data-tab-target="field-injections"]',
-          'a[data-tab-target="extensionElements"]',
-        ];
-        rendersAll(properties, 0);
+          'a[data-tab-target="extensionElements"]'
+        ]
+        rendersAll(properties, 0)
 
-        done();
-      });
-    });
+        done()
+      })
+    })
 
     it('renders all elments of an imported process diagram', function (done) {
-      renderPizza(data, done);
-    });
-  });
+      renderPizza(data, done)
+    })
+  })
 
   after(function () {
     Meteor.sendCoverage(function (/* stats, err */) {
-      window.open(`${Meteor.absoluteUrl()}/coverage`, coverageUrlId);
-    });
-  });
-});
+      window.open(`${Meteor.absoluteUrl()}/coverage`, coverageUrlId)
+    })
+  })
+})
